b2ce6aa7d8af45efb97280ad06cda7fe723d71ef
gao634 <gao634@purdue.edu>
2022-06-10 12:44:58 -0700

    first commit

--- Snake/snake.py
+++ Snake/snake.py
@@ -2,12 +2,33 @@ import pygame
 import random
 import math
 
+
 class cube(object):
-    rows = 0
-    w = 0
-    def __init__(self):
+    rows = 20
+    w = 500
+    def __init__(self, start, color = (255, 0, 0)):
+        #pos is coordinate tuple
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    def draw(self, surface, eyes = False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface, self.color, (i*dis + 1, j*dis, dis-2, dis-2))
+
 class snake(object):
+    # list of cube objects
     body = []
+    # 2d list of ordered pairs (dirnx, dirny) where every element represents a coordinate
     turns = []
     def __init__(self, color, pos):
         self.color = color
@@ -16,34 +37,49 @@ class snake(object):
         self.dirnx = 0
         self.dirny = 1
     def move(self):
-        for event in pygame.event.ege():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quite()
 
             keys = pygame.key.get_pressed()
             for key in keys:
-                if key[pygame.K_LEFT]:
+                if keys[pygame.K_LEFT]:
                     self.dirnx = -1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                if key[pygame.K_RIGHT]:
+                if keys[pygame.K_RIGHT]:
                     self.dirnx = 1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                if key[pygame.K_UP]:
+                if keys[pygame.K_UP]:
                     self.dirnx = 0
                     self.dirny = -1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                if key[pygame.K_DOWN]:
+                if keys[pygame.K_DOWN]:
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
 
-        for i, c in enumerated(self.body):
+        for i, c in enumerate(self.body):
             p = c.pos[:]
             if p in self.turns:
                 turn = self.turns[p]
-                c.move(turn[0])
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows - 1: c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows - 1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows - 1)
+                else: c.move(c.dirnx, c.dirny)
+
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
 def drawGrid(w, rows, surface):
     sizeBtwn = w // rows
 
@@ -57,17 +93,18 @@ def drawGrid(w, rows, surface):
         pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
 
 def redrawWindow(surface):
-    global width, rows
+    global width, rows, s
     surface.fill((0, 0, 0))
+    s.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
 def main():
-    global width, rows
+    global width, rows, s
     width = 500
     rows = 20
     win = pygame.display.set_mode((width, width))
-    #s = snake((255, 0, 0), (10, 10))
+    s = snake((255, 0, 0), (10, 10))
     flag = True
 
     clock = pygame.time.Clock()
@@ -76,7 +113,7 @@ def main():
         pygame.time.delay(50)
         clock.tick(10)
         redrawWindow(win)
-
+        s.move()
     pass
 
 main()
