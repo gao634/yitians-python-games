99b75a792d6e9f9083870e2565008dca57c35774
gao634 <gao634@purdue.edu>
2022-07-06 12:10:42 -0700

    first commit

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -11,13 +11,13 @@ block_size = play_width / 10
 board = np.zeros((10, 20))
 
 #directions for shap generation
-S = [(-1, 0), (0, 1), (1, 1)]
-Z = [(-1, 1), (0, 1), (1, 0)]
-I = [(0, 1), (0, -1), (0, -2)]
-O = [(-1, 0), (-1, 1), (0, 1)]
-J = [(-1, -1), (0, -1), (0, 1)]
-L = [(0, 1), (0, -1), (-1, -1)]
-T = [(-1, 0), (1, 0), (0, 1)]
+S = [(-1, 0), (0, 1), (1, 1), (4, 18)]
+Z = [(-1, 1), (0, 1), (1, 0), (5, 18)]
+I = [(0, 1), (0, -1), (0, -2), (4, 18)]
+O = [(-1, 0), (-1, 1), (0, 1), (5, 18)]
+J = [(1, 1), (0, -1), (0, 1), (4, 18)]
+L = [(0, 1), (0, -1), (-1, 1), (5, 18)]
+T = [(-1, 0), (1, 0), (0, -1), (4, 19)]
 shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
 
@@ -27,15 +27,15 @@ class Piece(object):
         self.color = shape_colors[shape]
         self.shape = shapes[shape]
         self.orientation = (0, 1)
-        self.x = 4
-        self.y = 18
+        self.x = self.shape[3][0]
+        self.y = self.shape[3][1]
 
     def generation(self):
         board[self.x, self.y] = 1
-        board[self.x + self.shape[0][0], self.x + self.shape[0][1]] = 1
-        board[self.x + self.shape[1][0], self.x + self.shape[1][1]] = 1
-        board[self.x + self.shape[2][0], self.x + self.shape[2][1]] = 1
+        board[self.x + self.shape[0][0], self.y + self.shape[0][1]] = 2
+        board[self.x + self.shape[1][0], self.y + self.shape[1][1]] = 3
+        board[self.x + self.shape[2][0], self.y + self.shape[2][1]] = 4
 
-test = Piece(1)
+test = Piece(6)
 test.generation()
 print(board)
