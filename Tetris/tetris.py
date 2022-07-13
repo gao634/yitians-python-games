c8bd5da5df6fab634beaff84ec7b9d0d4291f3c8
gao634 <gao634@purdue.edu>
2022-07-13 16:21:17 -0700

    asdf

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,7 +1,8 @@
 import pygame
 import numpy as np
+
 pygame.init()
-#screen parameters
+# screen parameters
 screen_width = 800
 screen_height = 700
 play_width = 300
@@ -14,7 +15,7 @@ y_edge2 = screen_height - y_edge1
 
 board = np.zeros((10, 20))
 
-#directions for shap generation
+# directions for shap generation
 S = [(-1, 0), (0, 1), (1, 1), (4, 18)]
 Z = [(-1, 1), (0, 1), (1, 0), (5, 18)]
 I = [(0, 1), (0, -1), (0, -2), (4, 18)]
@@ -25,8 +26,10 @@ T = [(-1, 0), (1, 0), (0, -1), (4, 19)]
 shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
 
+
 class Piece(object):
     global board
+
     def __init__(self, shape_number):
         self.color = shape_colors[shape_number]
         self.shape = shapes[shape_number]
@@ -37,65 +40,101 @@ class Piece(object):
         self.placed = False
         self.generation()
 
+    def block_valid(self, x, y, flag):
+        if x < 0 or x > 9:
+            return False
+        if y < 0 or y > 19:
+            return False
+        if flag:
+            if board[x, y] != 0:
+                return False
+        return True
+
     def generation(self):
+
         num = self.shape_number + 1
-        board[self.x, self.y] = num
+        blocks = [0, 0, 0, 0]
+        blocks[0] = (self.x, self.y)
         if self.orientation[0] == 0:
-            board[self.x + self.shape[0][0] * self.orientation[1], self.y + self.shape[0][1] * self.orientation[1]] = num
-            board[self.x + self.shape[1][0] * self.orientation[1], self.y + self.shape[1][1] * self.orientation[1]] = num
-            board[self.x + self.shape[2][0] * self.orientation[1], self.y + self.shape[2][1] * self.orientation[1]] = num
+            blocks[1] = (
+                self.x + self.shape[0][0] * self.orientation[1], self.y + self.shape[0][1] * self.orientation[1])
+            blocks[2] = (
+                self.x + self.shape[1][0] * self.orientation[1], self.y + self.shape[1][1] * self.orientation[1])
+            blocks[3] = (
+                self.x + self.shape[2][0] * self.orientation[1], self.y + self.shape[2][1] * self.orientation[1])
         else:
-            board[self.x + self.shape[0][1] * self.orientation[0], self.y + self.shape[0][0] * self.orientation[0]] = num
-            board[self.x + self.shape[1][1] * self.orientation[0], self.y + self.shape[1][0] * self.orientation[0]] = num
-            board[self.x + self.shape[2][1] * self.orientation[0], self.y + self.shape[2][0] * self.orientation[0]] = num
+            blocks[1] = (
+                self.x + self.shape[0][1] * self.orientation[0], self.y + self.shape[0][0] * self.orientation[0])
+            blocks[2] = (
+                self.x + self.shape[1][1] * self.orientation[0], self.y + self.shape[1][0] * self.orientation[0])
+            blocks[3] = (
+                self.x + self.shape[2][1] * self.orientation[0], self.y + self.shape[2][0] * self.orientation[0])
+        check = True
+        for block in blocks:
+            if self.block_valid(block[0], block[1], True):
+                board[block] = num
+            else:
+                check = False
+        return check
 
     def delete(self):
-        board[self.x, self.y] = 0
+        blocks = [0, 0, 0, 0]
+        blocks[0] = (self.x, self.y)
         if self.orientation[0] == 0:
-            board[self.x + self.shape[0][0] * self.orientation[1], self.y + self.shape[0][1] * self.orientation[1]] = 0
-            board[self.x + self.shape[1][0] * self.orientation[1], self.y + self.shape[1][1] * self.orientation[1]] = 0
-            board[self.x + self.shape[2][0] * self.orientation[1], self.y + self.shape[2][1] * self.orientation[1]] = 0
+            blocks[1] = (
+                self.x + self.shape[0][0] * self.orientation[1], self.y + self.shape[0][1] * self.orientation[1])
+            blocks[2] = (
+                self.x + self.shape[1][0] * self.orientation[1], self.y + self.shape[1][1] * self.orientation[1])
+            blocks[3] = (
+                self.x + self.shape[2][0] * self.orientation[1], self.y + self.shape[2][1] * self.orientation[1])
         else:
-            board[self.x + self.shape[0][1] * self.orientation[0], self.y + self.shape[0][0] * self.orientation[0]] = 0
-            board[self.x + self.shape[1][1] * self.orientation[0], self.y + self.shape[1][0] * self.orientation[0]] = 0
-            board[self.x + self.shape[2][1] * self.orientation[0], self.y + self.shape[2][0] * self.orientation[0]] = 0
+            blocks[1] = (
+                self.x + self.shape[0][1] * self.orientation[0], self.y + self.shape[0][0] * self.orientation[0])
+            blocks[2] = (
+                self.x + self.shape[1][1] * self.orientation[0], self.y + self.shape[1][0] * self.orientation[0])
+            blocks[3] = (
+                self.x + self.shape[2][1] * self.orientation[0], self.y + self.shape[2][0] * self.orientation[0])
+        check = True
+        for block in blocks:
+            if self.block_valid(block[0], block[1], False):
+                board[block] = 0
+            else:
+                check = False
+        return check
 
     def left_turn(self):
-        print("left")
         self.delete()
         placer = self.orientation[0]
         self.orientation[0] = -self.orientation[1]
         self.orientation[1] = placer
-        self.generation()
+        if not self.generation():
+            self.right_turn()
 
     def right_turn(self):
         self.delete()
         placer = self.orientation[0]
         self.orientation[0] = self.orientation[1]
         self.orientation[1] = -placer
-        self.generation()
+        if not self.generation():
+            self.left_turn()
 
     def soft_drop(self):
         self.delete()
-        if self.y > 2:
-            self.y -= 1
-        if not self.valid():
+        self.y -= 1
+        if not self.generation():
             self.y += 1
-        self.generation()
 
     def place(self):
         self.delete()
-        while self.valid():
+        while self.generation():
+            self.delete()
             self.y -= 1
+
         self.y += 1
-        self.placed = true
-        self.generation()
-    def valid(self):
-        return True
+        self.placed = True
 
 
 def drawGrid(surface):
-
     x = x_edge1
     y = y_edge1
     pygame.draw.line(surface, (255, 255, 255), (x, y_edge1), (x, y_edge2))
@@ -109,11 +148,13 @@ def drawGrid(surface):
         y = y + block_size
         pygame.draw.line(surface, (255, 255, 255), (x_edge1, y), (x_edge2, y))
 
+
 def redrawWindow(surface):
     surface.fill((0, 0, 0))
     drawGrid(surface)
     draw_pieces()
     pygame.display.update()
+
 
 def input(piece):
     for event in pygame.event.get():
@@ -133,18 +174,18 @@ def input(piece):
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 pass
-                #piece.left_turn()
+                # piece.left_turn()
             if event.key == pygame.K_RIGHT:
                 pass
-                #piece.right_turn()
+                # piece.right_turn()
             if event.key == pygame.K_DOWN:
                 pass
-                #piece.place()
+                # piece.place()
             if event.key == pygame.K_SPACE:
                 pass
-                #piece.soft_drop()
-    #keys = pygame.key.get_pressed()
-    #for key in keys:
+                # piece.soft_drop()
+    # keys = pygame.key.get_pressed()
+    # for key in keys:
     #    if keys[pygame.K_LEFT]:
     #        piece.left_turn()
     #        #print("left")
@@ -158,24 +199,32 @@ def input(piece):
     #        piece.soft_drop()
     #        #print("down")
 
+
 def draw_pieces():
     for x in range(10):
         for y in range(20):
             if (int(board[x][y]) - 1) != -1:
                 draw_square(shape_colors[int(board[x][y]) - 1], x, 19 - y)
 
+
 def draw_square(color, x, y):
-    pygame.draw.rect(surface, color, (x * block_size + x_edge1 + 1, y * block_size + y_edge1 + 1, block_size - 1, block_size - 1))
+    pygame.draw.rect(surface, color,
+                     (x * block_size + x_edge1 + 1, y * block_size + y_edge1 + 1, block_size - 1, block_size - 1))
+
 
 flag = True
 current = Piece(2)
 surface = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+print(board)
+
+
 def main():
     while flag:
         clock.tick(100)
         redrawWindow(surface)
         input(current)
         pygame.display.update()
+
 
 main()
